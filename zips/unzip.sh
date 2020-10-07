ecc2d78023e570c90863e498255f039c18dfd1e7
IBAS0742 <1134098699@qq.com>
2020-10-07 17:17:54 +0800

    定时任务

--- zips/unzip.sh
+++ zips/unzip.sh
@@ -3,17 +3,17 @@
 homePath="/home/ubuntu/"
 staticPath="www.sunibas.cn.static/"
 if [ ! -x "${homePath}frp_0.22.0" ]; then
-  tar -xvf "${homePath}${staticPath}zips/frp_0.22.0_linux_386.tar.gz" -C "${homePath}frp_0.22.0"
+  tar -xvf "${homePath}${staticPath}zips/frp_0.22.0_linux_386.tar.gz" "${homePath}frp_0.22.0"
 fi
 if [ ! -x "${homePath}${staticPath}pages/geotrellis-docs" ]; then
-  tar -vxf "${homePath}${staticPath}zips/geotrellis-docs.tar" -C "${homePath}${staticPath}pages/geotrellis-docs"
+  tar -vxf "${homePath}${staticPath}zips/geotrellis-docs.tar" "${homePath}${staticPath}pages/geotrellis-docs"
 fi
 if [ ! -x "${homePath}${staticPath}pages/lgend" ]; then
-  tar -vxf "${homePath}${staticPath}zips/lgend.tar" -C "${homePath}${staticPath}pages/lgend"
+  tar -vxf "${homePath}${staticPath}zips/lgend.tar" "${homePath}${staticPath}pages/lgend"
 fi
 if [ ! -x "${homePath}${staticPath}pages/modis" ]; then
-  tar -vxf "${homePath}${staticPath}zips/modis.tar" -C "${homePath}${staticPath}pages/modis"
+  tar -vxf "${homePath}${staticPath}zips/modis.tar" "${homePath}${staticPath}pages/modis"
 fi
 if [ ! -x "${homePath}${staticPath}pages/utils" ]; then
-  tar -vxf "${homePath}${staticPath}zips/utils.tar" -C "${homePath}${staticPath}pages/utils"
+  tar -vxf "${homePath}${staticPath}zips/utils.tar" "${homePath}${staticPath}pages/utils"
 fi
