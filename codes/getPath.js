99d8e4df868a5f4ab21b119c954151358dd18a77
IBAS0742 <1134098699@qq.com>
2020-10-08 20:11:49 +0800

    定时任务

--- codes/getPath.js
+++ codes/getPath.js
@@ -2,9 +2,9 @@ const cwd = process.cwd();
 const path = require('path');
 
 const getStaticPath = () => {
-    const staticPath = "www.sunibas.cn.static/";
+    const staticPath = "www.sunibas.cn.static";
 
-    return path.join(cwd.substring(cwd.indexOf(staticPath)),staticPath)
+    return path.join(cwd.substring(0,cwd.indexOf(staticPath)),staticPath)
 }
 
 module.exports = {
